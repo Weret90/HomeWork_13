13922335cea34ce03f8a1fce60befa1e31076c89
Arkhip Ermolaev <apostol-ea@mail.ru>
2021-04-14 01:00:20 +0900

    Initial commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,58 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    private Race race;
+    private int speed;
+    private String name;
+    private CountDownLatch cdl;
+    private CountDownLatch cdl2;
+    private CyclicBarrier cb;
+    private static boolean winnerFound;
+
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public Car(Race race, int speed, CountDownLatch cdl, CountDownLatch cdl2, CyclicBarrier cb) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.cdl = cdl;
+        this.cdl2 = cdl2;
+        this.cb = cb;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(this.name + " готов");
+            cdl.countDown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            cb.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+        cdl2.countDown();
+        checkWinner(this);
+    }
+    private static synchronized void checkWinner(Car c) {
+        if (!winnerFound) {
+            System.out.println(c.name + " - WIN");
+            winnerFound = true;
+        }
+    }
+}
